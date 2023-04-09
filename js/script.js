8eb3c627a58c99df33e07d2c31e20f944953610d
Dev3773 <stepaniuk.sergiy@gmail.com>
2023-04-09 17:21:38 +0300

    End level five js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+class User {
 
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const mango = new User("Mango");
+console.log(mango); 
+
+
+class Child extends User {
+  constructor(name, age) {
+    super(name);
+    this.age = age;
+  }
+  add(a) {
+    this.age = a;
+  }
+
+}
+
+const baby = new Child("baby", 77777);
+
+console.log(baby);
+
+baby.a = 55555;
+console.log(baby);
