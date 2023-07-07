d3c048e38a14af6f27a90e325925eb1140fa2d05
Dev3773 <stepaniuk.sergiy@gmail.com>
2023-07-07 18:45:26 +0300

    Made two/two

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
-const x = 7;
-let i = 2;
+const arr = [7, 'hello', null, true, undefined, [1, 2, 3]];
 
-do {
-  console.log(`${i} < ${x}`);
-  i++;
-} while (i < x);
+// for (let i = 0; i < arr.length; i += 1) {
+  //   if (Array.isArray(arr[i])) {
+    //     for (let j = 0; j < arr[i].length; j += 1) {
+      //       console.log(arr[i][j]);
+      //     }
+      //     continue;
+      //   }
+      //   console.log(arr[i]);
+      // }
+      
+      // for (const a of arr) {
+        //   console.log(a);
+        // }
+        
+        // const str = "I like JS";
+        // console.log(str);
+        
+        // const arr = str.split(' ');
+        // console.log(arr);
+        
+        // const str2 = arr.join(' ')
+        // console.log(str2);
+        
+        // const x = arr.slice(2, 5);
+        // console.log(arr);
+        // console.log(x);
+        
+        // const arr3 = arr.concat(arr2, ["fjjdjdjd"]);
+        // console.log(arr3);
+        
+// const arr2 = [777, 555, 888, 'vasya', null, true, undefined];
+// console.log(arr2);
+// console.log(arr2.splice(0, 0,'Petya'));
+// console.log(arr2);
