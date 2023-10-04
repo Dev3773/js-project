4f4bcc5161cffefefb1a5a2a4baed135ef72a7c3
Dev3773 <stepaniuk.sergiy@gmail.com>
2023-10-04 20:55:56 +0300

    made branch tree-six

--- js/script.js
+++ js/script.js
@@ -1,76 +1,116 @@
-// const bookShelf = {
-//   books: ["java", "css", "html"],
+// /**
+//  * 
+//  * @param {*} weidth 
+//  * @param {*} height 
+//  * @returns {Number}  
+//  */
 
-//   getBooks() {
-//     return this.books;
+// function fn(weidth, height) {
+//   return weidth * height;
+// }
+
+// const area = fn(5, 7);
+// console.log(area);
+
+// const obj = {
+//   proName: 'Audi',
+//   age: 5,
+//   date: new Date(),
+// };
+
+// console.log(obj.date);
+
+// const myArray = [5, 7, 9, 3, 2, 8];
+
+// const twoArray = [50, 30, 80, 70];
+
+// const threeArray = [...twoArray, ...myArray]
+// console.log(threeArray);
+
+// const first = { a: 1, b: 2, c: 20 };
+// const second = { c: 3, d: 4, };
+
+// const third = { ...second, ...first };
+// console.log(third);
+
+// const x = function (first, two, ...args) {
+//   console.log(first);
+//   console.log(two);
+//   console.log(args);
+// }
+
+// x(1);
+// x(1, 2);
+// x(1, 2, 3, "four", "five");
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
 //   },
-
-//   addBook(bookName) {
-//     this.books.push(bookName);
+//   {
+//     title: "Cпокойных вод",
+//     author: "Роберт Шекли",
+//     rating: 8.51,
 //   },
+// ];
 
-//   removeBook(bookName) {
-//     const bookIndex = this.books.indexOf(bookName);
-//     this.books.splice(bookIndex, 1);
+// for (const { title, author, rating } of books) {
+
+//   console.log(title);
+//   console.log(author);
+//   console.log(rating);
+// }
+
+// const books = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   rating: 8.38,
+//   obj: {
+//     one: 1,
+//     two: 2,
+//     three: 3,
 //   }
 // };
 
-// console.log(bookShelf.getBooks());
+// const { title, author, rating, obj: { one, two, tree } } = books;
 
-// bookShelf.addBook("pyton");
-// console.log(bookShelf.getBooks());
+// console.log(two);
 
-// bookShelf.removeBook("html");
-// console.log(bookShelf.getBooks());
+// const arr = [10, 20, 30, 40];
+// const [, , , forty] = arr;
 
-// const animal = {
-// legs: 4,
-// };
+// console.log(forty);
 
-// const dog = Object.create(animal);
-// dog.name = "Bim";
-
-// console.log(animal);
-// console.log(dog);
- 
-// const book = {
-//   proName: 848,
-//   likes: 158,
-//   parcase: 8,
-//   raiting: 5.38,
-// };
-
-// const keys = Object.keys(book);
-
-// for (const key of keys) {
-//   console.log(key);
-//   console.log(book[key]);
+// function fan({
+//   age: 5, old: 50,
+// }) {
+//   console.log(age);
 // }
 
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.383,
-  },
-  {
-    title: "Спокойных вод",
-    author: "Роберт Шекли",
-    rating: 8.513,
-  },
-  {
-    title: "Сон смешного человека",
-    author: "Федор Достоевский",
-    rating: 7.753,
-  },
-];
+// fan();
 
-let total = 0;
+// function book({
+//   title: "The Last Kingdom",
+//   numberOfPages: 736,
+//   downloads: 10283,
+//   rating: 8.38,
+//   isPublic: true,
+// });
 
-for (const book of books) {
-  total += book.rating;
-  console.log(book.rating);
-}
+// book();
 
-const result = (total / books.length).toFixed(2);
-console.log(result);
+// function doStuffWithBook(book) {
+//   const { title, rating, isPublic } = book;
+//   console.log(title);
+//   console.log(rating);
+// }
+
+// doStuffWithBook({
+//   title: "The Last Kingdom",
+//   numberOfPages: 736,
+//   downloads: 10283,
+//   rating: 8.38,
+//   isPublic: true,
+// });
