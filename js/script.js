ef739f6c85a75958ce82764c902ffaad5e82f9b6
Dev3773 <stepaniuk.sergiy@gmail.com>
2023-10-22 16:17:12 +0300

    made branch

--- js/script.js
+++ js/script.js
@@ -1,63 +1,100 @@
-// const arr = [5, 7, 3, 8, 10, 15, 30, 5, 7, 8, 20, 30, 40, 50];
+// function urlGen(domain) {
+//   return function (url) {
+//     return `https://${url}.${domain}`;
+//   }
+// } 
 
-// const arrObj = [
-//   {name: 'bmw', model: 'x 6', age: 5, sellOn: true },
-//   {name: 'audi', model: 'a 7', age: 3, sellOn: false},
-//   {name: 'mersedes', model: 's500', age: 7, sellOn: true},
-//   {name: 'tesla', model: 'model x', age: 2, sellOn: false},
-//   {name: 'porshe', model: 'panomera', age: 1, sellOn: true },
-// ];
+// const comUrl = urlGen('com');
 
-// const result = arr.filter((item, idx, array) => !(array.indexOf(item) === idx));
-// console.log(result);
-
-// const age = a => a.reduce((acc, { age }) => acc + age, 0); 
-// console.log(age(arrObj));
-
-const tweets = [
-  { id: "000", likes: 5, tags: ["js", "nodejs"] },
-  { id: "001", likes: 2, tags: ["html", "css"] },
-  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-  { id: "003", likes: 8, tags: ["css", "react"] },
-  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
-
-const tags = (tweets) =>
-  tweets.reduce((acc, el) => {
-    acc.push(el.tags);
-    return acc;
-  }, [])
-  .flatMap(element => element)
-  .filter((item, idx, array) => array.indexOf(item) === idx)
-  .sort((a, b) => a.localeCompare(b));
-
-console.log(tags(tweets));
-
-// const ar = [5, 7, 30, 3, 4, 95, 8, 88];
-
-// const arTwo = [...ar].sort((a, b) => a - b);
-// console.log(arTwo);
-
-// const arrString = ['Alla', 'Andrey', 'Vika', 'Bodya', 'Serzh', 'Cebit', 'cebit'];
-
-// const resString = [...arrString].sort((a, b) => a.localeCompare(b));
-// console.log(resString);
-
-// const resTwoString = [...arrString].sort((a, b) => b.localeCompare(a));
-// console.log(resTwoString);
+// console.log(comUrl('google'));
+// console.log(comUrl('netflix'));
+// console.log(comUrl('github'));
 
 
-// const students = [
-//   { name: "Манго", score: 83 },
-//   { name: "Поли", score: 59 },
-//   { name: "Аякс", score: 37 },
-//   { name: "Киви", score: 94 },
-// ];
+// const user = {
+//   authors: ['JS', 'CSS'],
+//   getAuthors() {
+// return this.authors;
+//   },
+//   addAuthors(val) {
+//     this.authors.push(val);
+//   },
+// };
 
-// const valSort = [...students].sort((a, b) => a.score - b.score);
-// console.log(valSort);
+// console.log(user.getAuthors());
+// user.addAuthors('HTML');
+// console.log(user.getAuthors());
 
-// const strSort = [...students].sort((a, b) => a.name.localeCompare(b.name));
-// console.log(strSort);
+// function foo() {
+//   console.log(this);
+// };
 
+// foo();
+
+// const ob = {
+//   userName: 'Petya',
+//   metOb() {
+// console.log(this);
+//   },
+// };
+
+// ob.metOb();
+
+// function fun() {
+//   console.log('This is the object:', this);
+// }
+
+// const ob = {
+//   user: 'Vasya',
+// };
+
+// ob.met = fun;
+
+// ob.met();
+
+// function greetGuests(griting) {
+//   console.log(`${griting}, ${this.userName} !!!`);
+// } 
+
+// const obM = {
+//   userName: "Mango",
+// };
+
+// const obP = {
+//   userName: "Poly",
+// };
+
+// greetGuests.call(obM, "Walcome");
+// greetGuests.apply(obP, ["Walcome"]);
+
+// const b = greetGuests.bind(obM);
+// b("Walcome");
+
+
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName.bind(customer));
+
+
+
+// function greet(clientName) {
+//   return `${clientName}, добро пожаловать в «${this.service}».`;
+// }
+
+// const steam = {
+//   service: "Steam",
+// };
+
+// const result = greet.bind(steam);
+// console.log(result("Sergio"));
 
