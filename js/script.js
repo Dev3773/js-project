4ec7bcb34d233c4e3c0ac0c27aa94107909f6ebe
Dev3773 <stepaniuk.sergiy@gmail.com>
2024-03-17 22:11:57 +0200

    made level seven-one

--- js/script.js
+++ js/script.js
@@ -43,38 +43,118 @@
  
 
 
-window.addEventListener('keydown', onKeypress);
+// window.addEventListener('keydown', onKeypress);
 
-function onKeypress(event) {
-  console.log('event key: ', event.key);
-  console.log('event key: ', event.code);  
-}
-
-
-const submitRef = document.querySelector(".js-submit");
-
-const handClick = (event) => {
-  console.log(event);
-}
-
-submitRef.addEventListener('click', handClick);
+// function onKeypress(event) {
+//   console.log('event key: ', event.key);
+//   console.log('event key: ', event.code);  
+// }
 
 
-window.addEventListener("keydown", event => {
-  console.log(event.key);
-})
+// const submitRef = document.querySelector(".js-submit");
+
+// const handClick = (event) => {
+//   console.log(event);
+// }
+
+// submitRef.addEventListener('click', handClick);
+
+
+// window.addEventListener("keydown", event => {
+//   console.log(event.key);
+// })
 
 
 
-const formRef = document.querySelector(".form");
-formRef.addEventListener("submit", (event) => preventDefault());
+// const formRef = document.querySelector(".form");
+// formRef.addEventListener("submit", (event) => preventDefault());
 
-const handValue = (event) => {
-  spanRef.textContent = event.currentTarget.value;
-};
+// const handValue = (event) => {
+//   spanRef.textContent = event.currentTarget.value;
+// };
 
-const imputRef = document.querySelector(".text-input");
-const spanRef = document.querySelector(".span");
+// const imputRef = document.querySelector(".text-input");
+// const spanRef = document.querySelector(".span");
 
-imputRef.addEventListener("input", handValue);
+// imputRef.addEventListener("input", handValue);
+
+
+
+// const item = document.querySelector(".list--item");
+// console.log(item);
+
+// item.style.fontSize = '24px';
+// item.style.color = 'tomato';
+
+// const list = document.querySelector(".list");
+// console.log(list);
+
+// list.style.textTransform = 'uppercase';
+
+// const itemAll = document.querySelectorAll(".list--item");
+// console.log(itemAll);
+
+// itemAll.style.fontSize = '24px';
+
+// for (const item of itemAll) {
+//   item.style.fontSize = '24px';
+// }
+
+
+// const list = document.querySelectorAll(".list");
+// console.log(list);
+
+// list.style.color = "blue";
+
+// for (const item of list) {
+//   item.style.color = "blue";
+// }
+
+
+// const img = document.querySelector(".image");
+// console.log(img.alt);
+
+// img.alt = "Macbooook";
+// console.log(img.alt);
+
+// const textRef = document.querySelector(".text");
+// textRef.classList.add("cool", "great");
+// console.log(textRef.classList);
+// textRef.classList.replace("text", "oooooooo");
+
+
+// const btn = document.querySelector('.btn');
+
+// console.log(btn.dataset.action);
+// btn.dataset.role = "admin";
+// delete btn.dataset.role;
+
+
+// const title = document.createElement("h2");
+// title.classList.add("subtitle");
+// title.setAttribute("title", "Zagolovok");
+// title.dataset.action = "update";
+// title.textContent = "My subTitle !!!";
+// console.log(title);
+
+// const img = document.querySelector(".image");
+// img.after(title);
+// title.remove();
+
+// const art = document.querySelector(".article");
+// console.log(art.innerHTML);
+
+// const title = document.querySelector(".article .title");
+// console.log(title.innerHTML = '');
+
+const arr = ["HTML", "CSS", "JS", "REACT"];
+
+const list = document.querySelector(".list");
+
+const markup = arr.map((tech) => 
+  `<li class"list--item">I study ${tech} !!!</li>`).join(" ");
+
+// list.innerHTML = markup;
+
+list.insertAdjacentHTML("afterbegin", markup);
 
